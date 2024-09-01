6e9372f444c33b891c588c0721c305a11b4f3ef6
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2024-09-01 22:47:25 +0300

    Initial Commit

---
+++ SwiftySearch/SwiftySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftySearchApp.swift
+//  SwiftySearch
+//
+//  Created by Roman Tverdokhleb on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
